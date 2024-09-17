cb7831267b4deccdac7649e7e96f2687600ff7fa
lorirothermel <lori@rothermel1.com>
2024-09-16 22:59:52 -0400

    Initial Commit

---
+++ DogPics/DogPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogPicsApp.swift
+//  DogPics
+//
+//  Created by Lori Rothermel on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct DogPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
